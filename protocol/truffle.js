80c6003a06fb0ef8a920a138bda1c8a09c843ef1
padoc-cloud <franlebaocbrahves47@gmail.com>
2021-01-10 23:04:29 +0100

    add kovan and change optimizer runs

--- protocol/truffle.js
+++ protocol/truffle.js
@@ -76,6 +76,15 @@ module.exports = {
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
 
+    kovan: {
+        provider: () => new PrivateKeyProvider(privateKey, 'https://kovan.infura.io/v3/' + infuraId),
+        network_id: 42,       // rinkeby's id
+        gas: 5500000,        // rinkeby has a lower block limit than mainnet
+        confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+        timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+        skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      },
+
     // Useful for private networks
     // private: {
       // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
@@ -97,7 +106,7 @@ module.exports = {
       settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
           enabled: true,
-          runs: 200
+          runs: 999999
         }
       }
       //  evmVersion: "byzantium"
